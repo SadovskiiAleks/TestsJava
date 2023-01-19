12521be6fb4dcfb3b379cc08d67bd1e67bf13c7d
SadovskiiAleks <alekssadovskij@yandex.ru>
2023-01-19 11:00:18 +0300

    >>

---
+++ src/main/java/org/example/MyClass.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class MyClass {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int square(int a) {
+        return a * a;
+    }
+
+    public int cube(int a) {
+        return a * a * a;
+    }
+
+    public String toString(int a) {
+        String string = "Вводимое число " + a + " !";
+        return string;
+    }
+}
